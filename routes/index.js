f7dcdc01b1397e352c0b224bc78eaacfc3c128da
Tenrashi <pierreantoine77@gmail.com>
2017-11-20 16:45:37 +0100

    a2

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,27 @@
-
 var express = require('express');
 var router = express.Router();
 const multer  = require('multer');
 const upload = multer({ dest: 'tmp/' });
-const fs = require('fs'); 
+const fs = require('fs');
 
+/* GET home page. */
 router.get('/monupload', function(req, res, next) {
   res.render('index');
 });
 
-router.post('/monupload', upload.array('monupload'), function(req, res, next) {
-  // traitement du formulaire 
-  //fs.rename(req.files[10].path, 'tmp/' + req.files[10].originalname, function(){});
-  for (i = 0; i < req.files.length; i++) {  
-    if ((req.files[i].mimetype =='image/png') && (req.files[i].size < 3145728))  {
-      fs.rename('tmp/' + req.files[i].path, 'public/images/' + req.files[i].originalname, function(){
-        res.send('Sucess');
-      });
-    } else {
-      fs.unlink(req.files[i].path, function() {
-        res.send('Error');
-      }); 
-  }
-} 
-});
-
-module.exports = router;
+router.post('/monupload', upload.array('monup', 4), function(req, res, next) {
+	for (i = 0; i < req.files.length; i++) {  
+		if ((req.files[i].mimetype =='image/png'||req.files[i].mimetype =='image/jpeg') && (req.files[i].size < 3145728))  {
+			console.log(req.files[i]);
+			fs.rename(req.files[i].path, 'public/images/' + req.files[i].originalname);
+		} else {
+		  fs.unlink(req.files[i].path, function() {
+			res.send('Error');
+		  }); 
+	  }
+	} 			
+	res.send('Success');
+	
+	});
+	
+	module.exports = router;
